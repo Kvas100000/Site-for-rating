3d4e8cd135fd8360201799f2e63a6be44399eb43
Kvas100000 <sasa0500432532@gmail.com>
2026-03-17 20:16:22 +0200

    hotfix media

--- config/settings.py
+++ config/settings.py
@@ -138,5 +138,5 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 ALLOWED_HOSTS = ['.onrender.com']
